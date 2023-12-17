1c5e8fe791c656c33f8ac28c9a7e7865b0323995
yyyculers <mogumogumogupi5@gmail.com>
2023-12-17 14:39:34 +0900

    a

--- send_mail.py
+++ send_mail.py
@@ -1,8 +1,16 @@
 def send_mail(slots, scales, me):
     to_email = me['email']
     print(to_email)
-    # TODO
-    # ここにメール送信処理
-    # scalesにレビュー予約情報が入っている → sample_as_reviewee.json参照(レビュワーとしてのサンプルレスポンスまだない)
-    # slotsに自分の開けたスロット情報がはいっている。連続した時間帯でスロットいれても実際は15分長さで45分間隔になっている → sample_my_slots.json参照、サンプルは2023/12/17 16時~17時のスロットを開けています
-    # me → sample_me.json参照
+    
+    # scalesからプロジェクト名を抽出
+    project_names = []
+    for scale in scales:
+        if 'team' in scale and 'project_gitlab_path' in scale['team']:
+            project_path = scale['team']['project_gitlab_path']
+            project_name = project_path.split('/')[-1]
+            project_names.append(project_name)
+
+    # プロジェクト名のリストを確認するための出力（デバッグ用）
+    print("プロジェクト名:", project_names)
+    
+    # TODO: ここにメール送信処理を実装
