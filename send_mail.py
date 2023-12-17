91874d42ee396473ae44af01b488b16c76a471f6
yyyculers <mogumogumogupi5@gmail.com>
2023-12-17 14:54:21 +0900

    a

--- send_mail.py
+++ send_mail.py
@@ -1,37 +1,37 @@
-from Body_make import modify_calendar_url, date_format, process_data
-import smtplib
-from email.mime.text import MIMEText
-from Body_make import modify_calendar_url
-
 def send_mail(slots, scales, me):
     to_email = me['email']
     print(to_email)
     GMAIL_ADDRESS = 'kkobayashi12356@gmail.com'   # Gmailアドレス
     APP_PASSWORD = 'musl szie muey zien'  # アプリパスワード
 
+    # メール本文の初期化
+    MAIL_BODY = "メール本文（１行目）\n"
+
+    # scales内のデータをメール本文に組み込む
     url = "https://calendar.google.com/calendar/render?action=TEMPLATE&text=レビュー&details=&dates=20231112T140000/20231112T150000"
     for scale in scales:
         if 'begin_at' in scale:
-            print("begin_at:", scale['begin_at'])
             new_start_time = date_format(scale['begin_at'])
-            print("new_start_time:", new_start_time)
             modified_url = modify_calendar_url(url, new_start_time)
-            print("Modified URL:", modified_url)
+            MAIL_BODY += f"{new_start_time}: {modified_url}\n"
+        
+        if 'team' in scale and 'project_gitlab_path' in scale['team']:
+            project_path = scale['team']['project_gitlab_path']
+            project_name = project_path.split('/')[-1]
+            MAIL_BODY += f"プロジェクト名: {project_name}\n"
 
-            TO_MAIL = 'kkobayashi12356@gmail.com' # 送信先メールアドレス（ここも書き換え必須）
-            MAIL_TITLE = 'メールタイトル'
-            MAIL_BODY =  f'''メール本文（１行目）{modified_url}
-            メール本文の続き...'''
+    TO_MAIL = 'kkobayashi12356@gmail.com' # 送信先メールアドレス（ここも書き換え必須）
+    MAIL_TITLE = 'メールタイトル'
 
-            # メールの設定処理
-            msg = MIMEText(MAIL_BODY)    # メール本文（日本語が含まれるときは MIMEText必須）
-            msg['Subject'] = MAIL_TITLE  # メールタイトル
-            msg['From'] = GMAIL_ADDRESS  # 送信元メールアドレス
-            msg['To'] = TO_MAIL          # 送信先メールアドレス
+    # メールの設定処理
+    msg = MIMEText(MAIL_BODY)    # メール本文（日本語が含まれるときは MIMEText必須）
+    msg['Subject'] = MAIL_TITLE  # メールタイトル
+    msg['From'] = GMAIL_ADDRESS  # 送信元メールアドレス
+    msg['To'] = TO_MAIL          # 送信先メールアドレス
 
-            # メールの送信処理
-            smtp = smtplib.SMTP('smtp.gmail.com', 587)
-            smtp.starttls()
-            smtp.login(GMAIL_ADDRESS, APP_PASSWORD)
-            smtp.send_message(msg)
-            smtp.close()
+    # メールの送信処理
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.starttls()
+    smtp.login(GMAIL_ADDRESS, APP_PASSWORD)
+    smtp.send_message(msg)
+    smtp.close()
